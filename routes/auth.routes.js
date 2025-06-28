33b921fc505cee49db32effd70032a1dcf4fd63b
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-06-28 15:59:48 +0200

    modify user object to retrieve firstName & lastName

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,10 +18,11 @@ const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", (req, res, next) => {
-  const { email, password, name } = req.body;
+  console.log("body", req.body)
+  const { email, password, firstName, lastName } = req.body;
 
   // Check if email or password or name are provided as empty strings
-  if (email === "" || password === "" || name === "") {
+  if (email === "" || password === "" || firstName === "" || lastName === "") {
     res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
@@ -58,15 +59,16 @@ router.post("/signup", (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name });
+      return User.create({ email, password: hashedPassword, firstName, lastName });
     })
     .then((createdUser) => {
+      console.log("user", createdUser)
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email, firstName, lastName, _id } = createdUser;
 
       // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const user = { email, firstName, lastName, _id };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
