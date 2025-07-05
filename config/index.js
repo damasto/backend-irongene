2121905c23b973297eb14d52e497b85c6500f682
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-07-05 13:00:25 +0200

    enable cors for several origins

--- config/index.js
+++ config/index.js
@@ -15,6 +15,8 @@ const cors = require("cors");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 
+const allowedOrigins = [ "http://localhost:5173","https://irongene.netlify.app" ]
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -22,11 +24,22 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL]
-    })
-  );
+
+  const corsOptions = {
+    orgin: function(origin, callback) {
+      if(!origin) {
+        return callback(null, true)
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Origin blocked by CORS"))
+      }
+    },
+    credentials: true
+  }
+  app.use(cors(corsOptions))
 
   console.log("url", FRONTEND_URL)
 
