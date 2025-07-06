e41f4d5d90cda0d00c43c5d06c700be1d5192be2
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-07-06 19:40:32 +0200

    remove unused route. edit clinic route

--- routes/clinics.routes.js
+++ routes/clinics.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
 const Clinic = require("../models/Clinic.model")
 
 router.get("/", async(req,res, next) => {
@@ -24,6 +25,10 @@ router.put("/:clinicId", async (req, res, next) => {
     const {clinicId} = req.params
     const update = req.body
 
+     if(!mongoose.Types.ObjectId.isValid(clinicId)) {
+            return res.status(400).json({message: "Invalid ID format"})
+        }
+
     try {
         if(update.hasOwnProperty("procedures")) {
             const updateProcedures = await Clinic.findByIdAndUpdate(
@@ -44,6 +49,10 @@ router.put("/:clinicId", async (req, res, next) => {
 router.get("/:clinicId", async(req,res, next) => {
 
     const {clinicId} = req.params
+
+     if(!mongoose.Types.ObjectId.isValid(clinicId)) {
+            return res.status(400).json({message: "Invalid ID format"})
+        }
 
     try {
         const clinic = await Clinic.findById(clinicId);
@@ -67,16 +76,5 @@ router.get("/:clinicId/procedures", async(req,res, next) => {
     }
 });
 
-router.post("/:clinicId/booking", async(req,res, next) => {
-
-    const {clinicId} = req.params;
-    
-    try {
-        const newClinic = await Clinic.create(req.body);
-        res.status(201).json(newClinic)
-    } catch (err) {
-        next(err);
-    }
-});
 
 module.exports = router
